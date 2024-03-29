9fb4994ca504853bea3c543be6881ee8a40fa1f9
samfelix61 <samfelix61@gmail.com>
2024-03-29 23:48:09 +0300

    done with assignmnt

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(item => name.toLowerCase() === item.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(item => item.toLowerCase().indexOf(name.toLowerCase()) === 0)
+}
+function matchName(driver, names){
+    return  driver.filter((record) => record.name === names);
+}
